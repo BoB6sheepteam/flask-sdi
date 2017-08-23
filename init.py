f07f2eaa290b3673ec57aae97e940db4e3070d7c
m2jeong <mjhan.bob6@gmail.com>
2017-08-22 19:48:04 -0700

    Fix inity.py

--- init.py
+++ init.py
@@ -1,12 +1,39 @@
-from flask import Flask
-app = Flask(from flask import Flask
+from flask import Flask, render_template, redirect, url_for, request
 
-        app = Flask(__name__)
 
-        @app.route('/')
 
-        def index():
-           return "Hello World"
+app = Flask(__name__)
 
-        if __name__== "__main__":
-           app.run()
+
+
+@app.route('/', methods=['GET', 'POST'])
+
+def login():
+
+    error = None
+
+    if request.method == 'POST':
+
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+
+            error = 'Invalid Credentials. Please try again.'
+
+        else:
+
+            return redirect(url_for('secret'))
+
+    return render_template('login.html', error=error)
+
+
+
+@app.route('/secret')
+
+def secret():
+
+    return "This is a secret page!"
+
+
+
+if __name__=="__main__":
+
+	app.run()
